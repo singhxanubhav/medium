685be865faf4a52244bfb940ad6b45ae84cba613
Anubhav Singh <anubhavsinghbkj@gmail.com>
2025-03-29 23:40:02 +0530

    added 1 more changes

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -1,2 +1,1 @@
-// export const BACKEND_URL = "https://medium-alpha-two.vercel.app" 
-export const BACKEND_URL = "http://localhost:3000" 
+export const BACKEND_URL = "https://medium-alpha-two.vercel.app" 
